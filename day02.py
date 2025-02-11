fba55de77fe6942108b9307c705497f90593f171
seuntwoosoon <tnddm0927@naver.com>
2025-02-11 11:30:21 +0900

    ed

--- day02.py
+++ day02.py
@@ -1,10 +1,11 @@
 n = int(input("input num"))
 cnt = 0
-for i in range(1, n+1):
+for i in range(2, n):
     if (n%i == 0):
         cnt += 1
+        break
 
-if cnt == 2:
+if cnt == 1:
     print("prime")
 else:
     print("not prime")
